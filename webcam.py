93bd601958eee239207217316baef65182c9d45e
jasonIV <thuhtetaung.d2@gmail..com>
2019-10-16 18:54:59 +0630

    beta release

--- webcam.py
+++ webcam.py
@@ -4,37 +4,79 @@ import pyzbar.pyzbar as pyzbar
 from openpyxl import Workbook
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import urllib.request as req
+from urllib.error import URLError
+import time
+from playsound import playsound
 
-# workbook = Workbook()
-# ws = workbook.active
+workbook = Workbook()
+ws = workbook.active
 
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('barcodeID-spreadsheet.json', scope)
-client = gspread.authorize(creds)
-sheet = client.open('barcodeID spreadsheet').sheet1
+
 
 cap = cv2.VideoCapture(1)
 font = cv2.FONT_HERSHEY_PLAIN
 
+strippedData = " "
 
-while True:
+def interneton():
+    try:
+        req.urlopen('https://www.google.com', timeout=1)
+        return True
+    except URLError:
+        return False
+        
+def updategspreadsheet():
     _, frame = cap.read()
     fframe = cv2.flip(frame, 1)
     decodedObjects = pyzbar.decode(frame)
     for obj in decodedObjects:
-        # print('Type : ', obj.type)
-        # print("Data = ", str(obj.data).strip("b''"))
         strippedData = str(obj.data).strip("b''")
-        sheet.update_cell(1, 1, strippedData)
         cv2.putText(fframe, strippedData, (50, 50), font, 2, (255, 0, 0), 3)
-
-        # ws['A1'] = strippedData
-        # workbook.save("sample.xlsx")
-        # else:
-        #     pass     
-
     cv2.imshow("Frame", fframe)
+    return strippedData
 
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
+if interneton() == True:
+    client = gspread.authorize(creds)
+    sheet = client.open('barcodeID spreadsheet').sheet1
+    while True:
+        _, frame = cap.read()
+        fframe = cv2.flip(frame, 1)
+        decodedObjects = pyzbar.decode(frame)
+        for obj in decodedObjects:
+            strippedData = str(obj.data).strip("b''")
+            cv2.putText(fframe, strippedData, (50, 50), font, 2, (255, 0, 0), 3)
+        cv2.imshow("Frame", fframe)
+        if strippedData != " ": 
+            sheet.update_cell( 1, 1, strippedData)
+            ws['A1'] = strippedData
+            workbook.save("sample.xlsx")
+            playsound('i-demand-attention.mp3')
+            time.sleep(5)
+            strippedData = " "
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
+else: 
+    while True: 
+        _, frame = cap.read()
+        fframe = cv2.flip(frame, 1)
+        decodedObjects = pyzbar.decode(frame)
+        for obj in decodedObjects:
+            strippedData = str(obj.data).strip("b''")
+            cv2.putText(fframe, strippedData, (50, 50), font, 2, (255, 0, 0), 3)
+        cv2.imshow("Frame", fframe)
+        if strippedData != " ":
+            ws['A1'] = strippedData
+            workbook.save("sample.xlsx")
+            playsound('i-demand-attention.mp3')
+            time.sleep(5)
+            strippedData = " "
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
+
