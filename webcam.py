2792657aef695af7e85deb8ef0d40d25bc467103
jasonIV <thuhtetaung.d2@gmail..com>
2019-10-17 10:08:44 +0630

    update

--- webcam.py
+++ webcam.py
@@ -41,45 +41,32 @@ def updategspreadsheet():
     cv2.imshow("Frame", fframe)
     return strippedData
 
-if interneton() == True:
-    client = gspread.authorize(creds)
-    sheet = client.open('barcodeID spreadsheet').sheet1
-    while True:
-        _, frame = cap.read()
-        fframe = cv2.flip(frame, 1)
-        decodedObjects = pyzbar.decode(frame)
-        for obj in decodedObjects:
-            strippedData = str(obj.data).strip("b''")
-            cv2.putText(fframe, strippedData, (50, 50), font, 2, (255, 0, 0), 3)
-        cv2.imshow("Frame", fframe)
-        if strippedData != " ": 
-            sheet.update_cell( 1, 1, strippedData)
-            ws['A1'] = strippedData
-            workbook.save("sample.xlsx")
-            play(song)
-            time.sleep(5)
-            strippedData = " "
-        key = cv2.waitKey(1)
-        if key == 27:
-            break
 
-else: 
-    while True: 
-        _, frame = cap.read()
-        fframe = cv2.flip(frame, 1)
-        decodedObjects = pyzbar.decode(frame)
-        for obj in decodedObjects:
-            strippedData = str(obj.data).strip("b''")
-            cv2.putText(fframe, strippedData, (50, 50), font, 2, (255, 0, 0), 3)
-        cv2.imshow("Frame", fframe)
-        if strippedData != " ":
-            ws['A1'] = strippedData
-            workbook.save("sample.xlsx")
-            play(song)
-            time.sleep(5)
-            strippedData = " "
-        key = cv2.waitKey(1)
-        if key == 27:
-            break
+while True:
+    _, frame = cap.read()
+    fframe = cv2.flip(frame, 1)
+    decodedObjects = pyzbar.decode(frame)
+    for obj in decodedObjects:
+        strippedData = str(obj.data).strip("b''")
+        cv2.putText(fframe, strippedData, (50, 50), font, 2, (255, 0, 0), 3)
+    cv2.imshow("Frame", fframe)
+    if strippedData != " ": 
+        if interneton() == True:
+            client = gspread.authorize(creds)
+            sheet = client.open('barcodeID spreadsheet').sheet1
+            sheet.update_cell( 1, 1, strippedData)
+        else:
+            pass
+        ws['A1'] = strippedData
+        workbook.save("sample.xlsx")
+        play(song)
+        time.sleep(2)
+        strippedData = " "
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+
+
 
 
